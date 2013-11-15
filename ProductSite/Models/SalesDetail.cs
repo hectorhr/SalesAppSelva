31dcee7103186a98237a4733080450de7c12a787
Kairus <kairuspir@gmail.com>
2013-11-15 13:57:25 -0800

    salesdetail, salessummary commit

---
+++ ProductSite/Models/SalesDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProductSite.Models
+{
+    public class SalesDetail
+    {
+        public int SalesDetailID { get; set; }
+        public int SalesSummaryID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        [Column(TypeName = "Money")]
+        public decimal Price { get; set; }
+        [Column(TypeName = "DateTime2")]
+        public DateTime DateCreated { get; set; }
+        [Column(TypeName = "DateTime2")]
+        public DateTime DateModified { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+        public virtual SalesSummary SalesSummary { get; set; }
+    }
+}
