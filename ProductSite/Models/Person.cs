4112fb8e696536e489a9e603903b10e05d38b072
Hector <hector_r@preludesys.com>
2013-11-15 13:59:22 -0800

    no message

--- ProductSite/Models/Person.cs
+++ ProductSite/Models/Person.cs
@@ -9,16 +9,16 @@ namespace ProductSite.Models
     public class Person
     {
         public int PersonID { get; set; }
-        public string Prefix { get; set; }
+        public string? Prefix { get; set; }
         public string FirstName { get; set; }
-        public string Middle { get; set; }
+        public string? Middle { get; set; }
         public string LastName { get; set; }
-        public string Suffix { get; set; }
+        public string? Suffix { get; set; }
         public int AddressID { get; set; }
         public virtual Address Address { get; set; }
         [Column(TypeName = "DateTime2")]
         public DateTime DOB { get; set; }
-        public long Phone { get; set; }
+        public long? Phone { get; set; }
         public string Gender { get; set; }
     }
 }
